1b2e7ba96b833e4a484a4bf886cc41eae0752f5c
Adam Struck <adamjstruck@gmail.com>
2015-04-02 13:49:57 -0700

    added mail.php

--- mail.php
+++ mail.php
@@ -1,10 +1,24 @@
-<?php $name = $_POST['name'];
+<?php
+$name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "dmsequencing@gmail.com";
+
+$from = "From: Dmseq.org"
+$to = "dmsequencing@gmail.com";
 $subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+$body="From: $name \n Message: $message \n";
+?>
+
+<?php
+if ($_POST['submit']) {
+   if ($name != '' && $email != '') {
+      if (mail ($to, $subject, $body, $from)) {
+         echo '<p>Thank you for your email!</p>';
+      } else {
+      	 echo '<p>Oops! An error occured. Try sending your message again.</p>';
+      }
+   } else {
+      echo '<p>You need to fill in all required fields!</p>';
+   }
+} 
 ?>
